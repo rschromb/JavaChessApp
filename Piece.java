fea79f4d207fd56b85987aa7331a1aafe8500424
rschromb <50712348+rschromb@users.noreply.github.com>
2021-06-17 14:44:51 -0500

    Create Piece.java

---
+++ Piece.java
@@ -0,0 +1,33 @@
+public abstract class Piece 
+{
+    private boolean taken = false;
+    private boolean white = false;
+    //location - for later
+
+    public Piece(boolean white)
+    {
+
+    }
+
+    public boolean checkWhite(boolean white)
+    {
+        return this.white;
+    }
+
+    public boolean checkDead(boolean taken)
+    {
+        return this.taken;
+    }
+
+    public void setWhite(boolean white)
+    {
+        this.white = true;
+    }
+
+    public void setDead(boolean taken)
+    {
+        this.taken = true;
+    }
+
+
+}
