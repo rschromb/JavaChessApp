26b11674ff300f302743b96cbfe607b709d87131
rschromb <50712348+rschromb@users.noreply.github.com>
2021-06-25 19:27:30 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -4,6 +4,24 @@ public class Board
 
     public Board()
     {
+        chessBoard = new Square[8][8];
 
+        for(int i = 1; i < 9; i++)
+            for(int j = 1; j < 9; j++)
+            {
+                chessBoard[i - 1][j-1] = new Square(i, j, null);
+            }
+    }
+
+    public void showBoard()
+    {
+        for(int i = 0; i < 8; i++)
+            for(int j = 0; j < 8; j++)
+            {
+                System.out.print(chessBoard[i][j].getX(i));
+                System.out.print(chessBoard[i][j].getY(j));
+                if(j == 7)
+                System.out.println();
+            }
     }
 }
