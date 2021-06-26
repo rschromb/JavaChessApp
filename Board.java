9b6a366c965530c0070faf6a1623206af875f282
rschromb <50712348+rschromb@users.noreply.github.com>
2021-06-26 09:26:40 -0500

    Update Board.java
    Added very rough interface for both white and black viewing. Building and setting pieces next.

--- Board.java
+++ Board.java
@@ -13,15 +13,54 @@ public class Board
             }
     }
 
-    public void showBoard()
+    public void showBlackView()
     {
         for(int i = 0; i < 8; i++)
-            for(int j = 0; j < 8; j++)
+            for(int j = 7; j >= 0; j--)
             {
-                System.out.print(chessBoard[i][j].getX(i));
+                System.out.print(convertToLetter(chessBoard[i][j].getX(i)));
                 System.out.print(chessBoard[i][j].getY(j));
-                if(j == 7)
+                if(j == 0)
                 System.out.println();
             }
+    }
+
+
+   public void showWhiteView()
+   {
+        for(int i = 7; i >= 0; i--)
+            for(int j = 0; j < 8; j++)
+            {
+            System.out.print(convertToLetter(chessBoard[i][j].getX(i)));
+            System.out.print(chessBoard[i][j].getY(j));
+            if(j == 7)
+            System.out.println();
+            }
+    }
+    
+    private String convertToLetter(int x)
+    {
+        int input = x;
+        switch(input)
+        {
+            case 1:
+            return "A";
+            case 2:
+            return "B";
+            case 3:
+            return "C";
+            case 4:
+            return "D";
+            case 5:
+            return "E";
+            case 6:
+            return "F";
+            case 7:
+            return "G";
+            case 8:
+            return "H";
+            default:
+            return "Error";
+        }
     }
 }
