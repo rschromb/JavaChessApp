e5575b63d8c4ad48f25c48d1ea1d51261099a1bd
rschromb <50712348+rschromb@users.noreply.github.com>
2021-06-26 09:33:36 -0500

    Update Chess.java
    Using simple terminal interface to get basic board and piece mechanics going before converting to a more sophisticated GUI model.

--- Chess.java
+++ Chess.java
@@ -1,5 +1,10 @@
 public class Chess {
     public static void main(String[] args) throws Exception {
-        System.out.println("This is my chess program!");
+        //System.out.println("This is my chess program!");
+        Board chessGame = new Board();
+
+        chessGame.showBlackView();
+        System.out.println();
+        chessGame.showWhiteView();
     }
 }
