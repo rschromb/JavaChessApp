5403a593146b6adafed7113d57864ebdf447bbc9
rschromb <50712348+rschromb@users.noreply.github.com>
2021-07-12 10:18:33 -0500

    Update Chess.java
    Updated main to display the GUI instead of returning output to terminal.

--- Chess.java
+++ Chess.java
@@ -1,10 +1,10 @@
-public class Chess {
-    public static void main(String[] args) throws Exception {
+package JavaChessApp;
+
+public class Chess 
+{
+    public static void main(String[] args) throws Exception 
+    {
         //System.out.println("This is my chess program!");
         Board chessGame = new Board();
-
-        chessGame.showBlackView();
-        System.out.println();
-        chessGame.showWhiteView();
     }
 }
