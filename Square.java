95a12d245eae4739dac7444f1a355aadd0b4f45e
rschromb <50712348+rschromb@users.noreply.github.com>
2021-07-21 09:14:22 -0500

    Update Square.java
    Removed redundant methods that were causing error in GridLayout display. Thank you StrangeQuark for finding this solution for me!

--- Square.java
+++ Square.java
@@ -49,26 +49,6 @@ public class Square extends JButton implements ActionListener
 
     }
 
-    public void setX(int x)
-    {
-        this.x = x;
-    }
-
-    public void setY(int y)
-    {
-        this.y = y;
-    }
-
-    public int getX()
-    {
-        return x;
-    }
-
-    public int getY()
-    {
-        return y;
-    }
-
     public void setPiece(Piece x)
     {
         this.piece = x;
